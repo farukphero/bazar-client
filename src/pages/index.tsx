ae2123c95f108c0b4a920b4f6619c5233b8cccc5
Abdullah Miraj <miraj2465@gmail.com>
2023-09-08 01:00:55 +0600

    common questions section added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,18 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import { StickyNavbar } from './components/shared/NavBar/NavBar'
-import Banner from './components/Banner/Banner'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import { StickyNavbar } from "./components/shared/NavBar/NavBar";
+import Banner from "./components/Banner/Banner";
+import FaqSection from "./components/CommonQns/FaqSection";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between  ${inter.className}`}
     >
-    <Banner></Banner>
+      <Banner></Banner>
+      <FaqSection />
     </main>
-  )
+  );
 }
