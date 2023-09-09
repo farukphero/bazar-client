fc5360f6641d965622be124556f2664b0237030a
faridulhaque <faridulhaquemurshed@gmail.com>
2023-09-09 19:54:29 +0600

    done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,16 +3,27 @@ import { Inter } from "next/font/google";
 import { StickyNavbar } from "./components/shared/NavBar/NavBar";
 import Banner from "./components/Banner/Banner";
 import FaqSection from "./components/CommonQns/FaqSection";
+import ProductCart from "./components/Cart/ProductCart";
+import { useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [openCart, setOpenCart] = useState(false);
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between  ${inter.className}`}
     >
+      <button
+        className="btn bg-primary text-white fixed right-0 m-auto"
+        onClick={() => setOpenCart(!openCart)}
+      >
+        view cart
+      </button>
       <Banner></Banner>
       <FaqSection />
+      <ProductCart openCart={openCart} setOpenCart={setOpenCart}></ProductCart>
     </main>
   );
 }
