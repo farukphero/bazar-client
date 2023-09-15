4504bacf5f37380e6372cc0e35170a5dff1efb56
Mostafizur <mostafizurdh2000@gmail.com>
2023-09-15 15:22:52 +0600

    big fix 1

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useRef } from "react";
 import SearchBar from "../Shared/SearchBars/SearchBars";
 import useTop from "@/hooks/useTop";
 import { useSelector } from "react-redux";
