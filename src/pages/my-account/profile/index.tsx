7d8f87d3a32fa808cb24ba3e9b3915a602fecaad
Abdullah Miraj <miraj2465@gmail.com>
2023-09-27 21:13:13 +0600

    footer added

--- src/pages/my-account/profile/index.tsx
+++ src/pages/my-account/profile/index.tsx
@@ -1,10 +1,13 @@
 import React, { ReactElement } from "react";
 import MyAccountLayout from "@/layouts/MyAccountLayout/MyAccountLayout";
+import { Typography } from "@material-tailwind/react";
 
 const Profile = () => {
   return (
     <MyAccountLayout>
-      <div>My profile</div>
+      <Typography variant="h1" style={{ margin: "32px auto" }}>
+        My profile
+      </Typography>
     </MyAccountLayout>
   );
 };
